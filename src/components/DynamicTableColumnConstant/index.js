367c400b1c40cfa4e9a0399054b9d4ba81d5a43a
Shivam Govind Rao <shivamgrao9161@gmail.com>
2024-11-01 21:40:43 +0530

    added lab table

--- src/components/DynamicTableColumnConstant/index.js
+++ src/components/DynamicTableColumnConstant/index.js
@@ -141,15 +141,33 @@ export const motorColumns = [
 ];
 
 export const labColumns = [
-  { title: "Subject ID", dataIndex: "subjectId", key: "subjectId" },
-  { title: "Stay ID", dataIndex: "stayId", key: "stayId" },
-  { title: "Lab Test", dataIndex: "testName", key: "testName" },
-  { title: "Result", dataIndex: "result", key: "result" },
-  { title: "Unit", dataIndex: "unit", key: "unit" },
-  { title: "Date", dataIndex: "date", key: "date" },
-  { title: "Time", dataIndex: "time", key: "time" },
+  {
+    title: "Chart Time",
+    dataIndex: "charttime",
+    key: "charttime",
+    render: (charttime) => dayjs(charttime).format("YYYY-MM-DD HH:mm a"),
+  },
+  {
+    title: "Label",
+    dataIndex: "label",
+    key: "label",
+  },
+  {
+    title: "Value",
+    dataIndex: "value",
+    key: "value",
+  },
+  {
+    title: "Value Number",
+    dataIndex: "valuenum",
+    key: "valuenum",
+  },
+  {
+    title: "Unit of Measurement",
+    dataIndex: "valueuom",
+    key: "valueuom",
+  },
 ];
-
 
 export const ventilationSettingsColumns = [
   {
